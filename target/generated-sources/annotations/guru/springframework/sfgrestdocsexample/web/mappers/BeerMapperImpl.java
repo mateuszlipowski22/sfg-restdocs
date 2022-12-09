b99e75ca127b412db10ec5b258e6498d9d8a2dd2
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-09 21:48:12 +0100

    Initial commit

---
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgrestdocsexample.web.mappers;
+
+import guru.springframework.sfgrestdocsexample.domain.Beer;
+import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-12-09T21:47:48+0100",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 19.0.1 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto BeerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        return beerDto;
+    }
+
+    @Override
+    public Beer BeerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Beer beer = new Beer();
+
+        return beer;
+    }
+}
